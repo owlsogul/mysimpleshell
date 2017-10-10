72c0de4fe9f6c8a97a9217bafca4e2778ef5b121
owlsogul <owlsogul@naver.com>
2017-10-10 22:44:20 +0900

    last commit

--- mysimpleshell.c
+++ mysimpleshell.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#pragma warning(disable : 4996)
+//#pragma warning(disable : 4996)
 
 #include <sys/types.h> // to use variable type of system
 #include <unistd.h> // to use system call
@@ -9,9 +9,9 @@
 #include <sys/wait.h>
 
 void showPrompt();
-void receiveCommand(char** cmd, char*** argv, int* argc);
-void initCommand(char** cmd, char*** argv, int* argc);
-void processCommand(char** cmd, char*** argv);
+void receiveCommand(char*** argv, int* argc);
+void initCommand(char*** argv, int* argc);
+void processCommand(char*** argv, int* argc);
 
 #define true 1
 #define false 0
@@ -19,51 +19,52 @@ typedef int boolean;
 
 #define MAX_INPUT_SIZE 100
 
-#define DEBUG
+#define DEBUG_NO
 
 int main(){
 
   boolean shellLoop = true;
-  char *cmd = NULL, **argv = NULL;
+  char **argv = NULL;
   int argc = 0;
 
   while (shellLoop){
     showPrompt();
-    receiveCommand(&cmd, &argv, &argc);
+    receiveCommand(&argv, &argc);
     #ifdef DEBUG
-    printf("my Command is : %s\n", cmd);
     printf("Arguments... ");
     for (int i = 0; i < argc; i++){
       printf("%s,", argv[i]);
     }
     printf("\n");
     #endif
-    processCommand(&cmd, &argv);
-    initCommand(&cmd, &argv, &argc);
+    processCommand(&argv, &argc);
+    initCommand(&argv, &argc);
   }
   return 0;
 }
 
 void showPrompt(){
-  printf("mingyu$");
+  printf("mingyu$ ");
   return;
 }
 
-void receiveCommand(char** cmd, char*** argv, int* argc){
+void receiveCommand(char*** argv, int* argc){
 
   //
   // input part
   //
   char input[MAX_INPUT_SIZE] = ""; // array saved inputs
   char seps[3] = " "; // tokenizer seperator
-  fgets(input, MAX_INPUT_SIZE, stdin); // for security, I must use fgets than gets
+  do {
+    fgets(input, MAX_INPUT_SIZE, stdin); // for security, I must use fgets than gets
+  } while(strlen(input) == 1); // catch the case that user only type enter
 
   //
   // counting number of token(argument)
   //
-  int sizeOfInput = 0;
-  int numberOfBlank = 0;
-  boolean isBlankBefore = false;
+  int sizeOfInput = 0; // size of input(to iterate)
+  int numberOfBlank = 0; // how many blank in input string
+  boolean isBlankBefore = false; // to find pattern 'blank char blank'
   while(input[sizeOfInput] != '\0'){
     if (isBlankBefore == false && input[sizeOfInput] == ' '){
       numberOfBlank++;
@@ -74,21 +75,18 @@ void receiveCommand(char** cmd, char*** argv, int* argc){
     }
     sizeOfInput++;
   }
+  input[sizeOfInput-1] = '\0';
+  numberOfBlank += 1; // consider command
 
   //
   // sepertate command and argument
   //
+  *argv = (char**)malloc(sizeof(char*) * (numberOfBlank));
   char* token;
   token = strtok(input, seps);
-  // malloc cmd, argument
-  *cmd = (char*)malloc(sizeof(char) * strlen(token));
-  *argv = (char**)malloc(sizeof(char*) * (numberOfBlank));
-
-  strcpy(*cmd, token);
-  token = strtok(NULL, seps);
   int argIndex = 0;
-  while (token != NULL){
-    (*argv)[argIndex] = (char*)malloc(sizeof(char*) * strlen(token));
+  while(token != NULL){
+    (*argv)[argIndex] = (char*)malloc(strlen(token)+1);
     strcpy((*argv)[argIndex], token);
     token = strtok(NULL, seps);
     argIndex++;
@@ -97,27 +95,37 @@ void receiveCommand(char** cmd, char*** argv, int* argc){
   return;
 }
 
-void initCommand(char** cmd, char*** argv, int* argc){
-  //free(*cmd); why error occur?
+void initCommand(char*** argv, int* argc){
   for (int i = 0; i < *argc; i++){
     free((*argv)[i]);
   }
   free(*argv);
-  *cmd = NULL;
   *argv = NULL;
   *argc = 0;
 }
 
-void processCommand(char** cmd, char*** argv){
+void processCommand(char*** argv, int* argc){
+  #ifdef DEBUG
+    printf("Arguments... ");
+    for (int i = 0; i < *argc; i++){
+      printf("%s.", (*argv)[i]);
+    }
+    printf("\n");
+  #endif
+  // exit code
+  if (strcmp((*argv)[0],"exit") == 0){
+    exit(0);
+  }
   pid_t pid;
-  int status;
+  int status = 1;
   pid = fork();
   if (pid < 0){
     // some error occur when make child process
+    status = -1;
   }
   else if (pid == 0){
     // child process
-    execve(*cmd, *argv, 0);
+    execvp((*argv)[0], *argv);
   }
   else {
     // parent process
@@ -127,7 +135,7 @@ void processCommand(char** cmd, char*** argv){
       status = -1;
       break;
     }
-    printf("Child Process is completed");
+    printf("Child Process is completed\n");
   }
   return;
 }
