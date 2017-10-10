e4277bb2b196a308d51e1b5d5d5c83569d7c6d94
Jo Mingyu <owlsogul@gmail.com>
2017-10-10 19:23:02 +0900

    i/o task complete

---
+++ mysimpleshell.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable : 4996)
+
+#include <sys/types.h> // to use variable type of system
+#include <unistd.h> // to use system call
+
+void showPrompt();
+void receiveCommand(char** cmd, char*** argv, int* argc);
+void initCommand(char** cmd, char*** argv, int* argc);
+void pidCreate(pid_t* pid);
+void processPid(pid_t* pid);
+
+#define true 1
+#define false 0
+typedef int boolean;
+
+#define MAX_INPUT_SIZE 100
+
+int main(){
+
+  boolean shellLoop = true;
+  char *cmd = NULL, **argv = NULL;
+  int argc = 0;
+  pid_t pid = NULL;
+
+  while (shellLoop){
+    showPrompt();
+    receiveCommand(&cmd, &argv, &argc);
+    initCommand(&cmd, &argv, &argc);
+  }
+  return 0;
+}
+
+void showPrompt(){
+  printf("mingyu$");
+  return;
+}
+
+void receiveCommand(char** cmd, char*** argv, int* argc){
+
+  //
+  // input part
+  //
+  char input[MAX_INPUT_SIZE] = ""; // array saved inputs
+  char seps[3] = " "; // tokenizer seperator
+  fgets(input, MAX_INPUT_SIZE, stdin); // for security, I must use fgets than gets
+
+  //
+  // counting number of token(argument)
+  //
+  int sizeOfInput = 0;
+  int numberOfBlank = 0;
+  boolean isBlankBefore = false;
+  while(input[sizeOfInput] != '\0'){
+    if (isBlankBefore == false && input[sizeOfInput] == ' '){
+      numberOfBlank++;
+      isBlankBefore = true;
+    }
+    else if (isBlankBefore == true && input[sizeOfInput] != ' '){
+      isBlankBefore = false;
+    }
+    sizeOfInput++;
+  }
+
+  //
+  // sepertate command and argument
+  //
+  char* token;
+  token = strtok(input, seps);
+  // malloc cmd, argument
+  *cmd = (char*)malloc(sizeof(char) * strlen(token));
+  *argv = (char**)malloc(sizeof(char*) * (numberOfBlank));
+
+  strcpy(*cmd, token);
+  token = strtok(NULL, seps);
+  int argIndex = 0;
+  while (token != NULL){
+    (*argv)[argIndex] = (char*)malloc(sizeof(char*) * strlen(token));
+    strcpy((*argv)[argIndex], token);
+    token = strtok(NULL, seps);
+    argIndex++;
+  }
+  *argc = numberOfBlank;
+  return;
+}
+
+void initCommand(char** cmd, char*** argv, int* argc){
+  //free(*cmd); why error occur?
+  for (int i = 0; i < *argc; i++){
+    free((*argv)[i]);
+  }
+  free(*argv);
+  *cmd = NULL;
+  *argv = NULL;
+  *argc = NULL;
+}
